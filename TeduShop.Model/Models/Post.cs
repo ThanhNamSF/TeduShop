d3969822eb476384409ee39a7fa7bee0fa4f35ff
PANNAY\ADMIN64 <fsthanhnamsf@gmail.com>
2017-02-20 22:17:05 +0700

    Update

---
+++ TeduShop.Model/Models/Post.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using TeduShop.Model.Abstract;
+
+namespace TeduShop.Model.Models
+{
+    [Table("Posts")]
+    public class Post : Auditable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Column(TypeName = "varchar")]
+        [Required]
+        public string Alias { get; set; }
+
+        public int CategoryID { get; set; }
+
+        [MaxLength(500)]
+        public string Image { get; set; }
+
+        [MaxLength(500)]
+        public string Description { get; set; }
+
+        [MaxLength(500)]
+        public string Content { get; set; }
+
+        public bool? HomeFlag { get; set; }
+
+        public bool? HotFlag { get; set; }
+
+        public int? ViewCount { get; set; }
+
+        [ForeignKey("CategoryID")]
+        public virtual PostCategory PostCategory { get; set; }
+
+        public virtual IEnumerable<PostTag> PostTags { get; set; }
+    }
+}
---
+++ TeduShop.Model/Models/Tag.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Models
+{
+    [Table("Tags")]
+    public class Tag
+    {
+        [Key]
+        [MaxLength(50)]
+        public string ID { get; set; }
+
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [MaxLength(50)]
+        public string Type { get; set; }
+
+        public virtual IEnumerable<ProductTag> ProductTags { get; set; }
+
+        public virtual IEnumerable<PostTag> PostTags { get; set; }
+    }
+}
